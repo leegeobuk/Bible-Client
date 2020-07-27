e477582e539551e74381d0db375d79231e677f1c
leegeobuk <leegeobuk@gmail.com>
2020-07-28 03:18:29 +0900

    Add Book component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Chapter from './script/chapter';
+import Book from './script/book';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>주께로 향하는 삶</h1>
-      <h3>Life Inclined to God</h3>
-      <Chapter />
-    </div>
-  );
+const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alpha/"
+
+class App extends React.Component {
+  state = {
+    books: []
+  }
+
+  getBooks = async () => {
+    const books = await axios.get(baseUrl)
+    this.setState({books: books.data.books})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>주께로 향하는 삶</h1>
+        <h3>Life Inclined to God</h3>
+        {this.state.books.map((book, i) => {
+          return <Book key={i} book={book} />
+        })}
+        <Chapter />
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
 }
 
 export default App;
