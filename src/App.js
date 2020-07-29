c7166e44ba0b11183dc13b1ccddf9eb6b52f4651
leegeobuk <leegeobuk@gmail.com>
2020-07-30 04:01:15 +0900

    Added Routes to Home and Script

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import Book from './components/book';
+import Home from "./routes/Home";
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
+import Script from './routes/Script';
 import axios from 'axios';
 
 const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alpha/"
 
 class App extends React.Component {
   state = {
+    version: "KRV",
     books: []
   }
 
   getBooks = async () => {
     const books = await axios.get(baseUrl)
-    this.setState({books: books.data.books})
+    this.setState(current => ({version: current.version, books: books.data.books}))
   }
   
   render() {
+    const {version, books} = this.state
     return (
       <div className="App">
-        <h1>주께로 향하는 삶</h1>
-        <h3>Life Inclined to God</h3>
-        {this.state.books.map((book, i) => {
-          return <Book key={i} book={book} />
-        })}
+        <HashRouter>
+          <Link to="/">
+            Home
+          </Link>
+          <Switch>
+            <Route exact path={"/"}>
+              <Home version={version} books={books} />
+            </Route>
+            {books.map((book, i) => {
+              return <Route path={`/${version}/${book.book_name}/:chapter`} component={Script} key={i} />
+            })}
+          </Switch>
+        </HashRouter>
       </div>
     )
   }
