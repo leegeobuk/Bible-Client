0413b99b28bcd7b23eb20c26057a7b74ce7ee0e0
leegeobuk <leegeobuk@gmail.com>
2020-07-30 03:58:27 +0900

    Add About component

---
+++ src/routes/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function About() {
+  return (
+    <span>
+      이 웹사이트를 주님께 받칩니다.
+      언제나 저를 지켜주시고 제 삶에 기쁨이 되어주셔서 감사합니다.
+      더욱 많은 사람들이 하나님의 말씀을 읽고 깨닫길 소망합니다.
+      나의 사랑하는 어머니 평생 주님과 동행하게 하소서.
+      <strong>
+        하나님이 세상을 이처럼 사랑하사 독생자를 주셨으니
+        이는 그를 믿는 자마다 멸망하지 않고 영생을 얻게 하려 하심이라 (요 3:16)
+      </strong>
+
+      I lift up this website to my Lord.
+      Thank you Lord for being protector of my life and greatest joy of all.
+      I pray more people read the Bible and realize it.
+      Let my mother, person who I love most and who loves me most, to walk with you forever.
+      <strong>
+        For God so loved the world, that he gave his only Son, 
+        that whoever believes in him should not perish but have eternal life. (John 3:16)
+      </strong>
+    </span>
+  )
+}
+
+export default About
