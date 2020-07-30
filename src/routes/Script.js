efd1ce21bd4d8204385696b16d31809ed73b839b
leegeobuk <leegeobuk@gmail.com>
2020-07-31 01:43:32 +0900

    Add componentDidUpdate

--- src/routes/Script.js
+++ src/routes/Script.js
@@ -1,40 +1,52 @@
 import React from "react";
 import axios from "axios";
 import Verse from "../components/Verse";
+import ChapterNav from "../components/ChapterNav";
 
 const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alpha/"
 
 class Script extends React.Component {
   state = {
-    version: "KRV",
-    bookId: "Gen",
-    chapter: "1",
     passages: []
   }
 
   getScriptByChapter = async () => {
-    console.log(this.props)
-    const {version, bookId, chapter} = this.props.location.state
-    const script = await axios.get(baseUrl + `${version}/${bookId}/${chapter}`)
+    const {version, book, chapter} = this.props.location.state
+    const script = await axios.get(baseUrl + `${version}/${book.book_id}/${chapter}`)
 
     this.setState({
       passages: script.data.passages
     })
   }
 
-  render() {
-    const {passages} = this.state
-    return (
-      <div>
-        {passages.map((passage, i) => {
-          return <Verse key={i} passage={passage} />
-        })}
-      </div>
-    )
-  }
-
   componentDidMount() {
     this.getScriptByChapter();
+  }
+  
+  componentDidUpdate() {
+    this.getScriptByChapter();
+  }
+
+  componentWillUnmount() {
+    
+  }
+
+  render() {
+    const {passages} = this.state
+    const {version, book, chapter} = this.props.location.state
+    return (
+      <div>
+        <h1>
+          {`${book.book_name} ${chapter}장`}
+        </h1>
+        <ChapterNav version={version} book={book} />
+        <div>
+          {passages.map((passage, i) => {
+            return <Verse key={i} passage={passage} />
+          })}
+        </div>
+      </div>
+    )
   }
 }
 
