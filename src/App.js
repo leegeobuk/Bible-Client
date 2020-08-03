0c2be9e3eaf4b31bef36b2e76391b1e3959bd0e7
leegeobuk <leegeobuk@gmail.com>
2020-08-04 00:01:11 +0900

    Add general font-family and color for all a tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Home from "./routes/Home";
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Script from './routes/Script';
 import axios from 'axios';
 import MainNav from './components/MainNav';
 import About from './routes/About';
+import classes from './App.module.css';
 
 const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alpha/"
 
@@ -22,18 +23,16 @@ class App extends React.Component {
   render() {
     const {version, books} = this.state
     return (
-      <div className="App">
+      <div className={classes.App}>
         <HashRouter>
           <MainNav />
-          <Switch>
-            <Route exact path={"/"}>
-              <Home version={version} books={books} />
-            </Route>
-            <Route exact path={"/about"} component={About} />
-            {books.map((book, i) => {
-              return <Route path={`/${version}/${book.book_name}/:chapter`} component={Script} key={i} />
-            })}
-          </Switch>
+          <Route exact path={"/"}>
+            <Home version={version} books={books} />
+          </Route>
+          <Route exact path={"/about"} component={About} />
+          {books.map((book, i) => {
+            return <Route path={`/${version}/${book.book_name}/:chapter`} component={Script} key={i} />
+          })}
         </HashRouter>
       </div>
     )
