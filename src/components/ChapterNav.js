2971337d833299324bd5526420351d02b80197ef
leegeobuk <leegeobuk@gmail.com>
2020-08-04 00:01:47 +0900

    Add .active, hover effect

--- src/components/ChapterNav.js
+++ src/components/ChapterNav.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import { NavLink } from "react-router-dom";
+import classes from "./ChapterNav.module.css";
 
-class ChapterNav extends React.Component {
+class ChapterNav extends Component {
   render() {
-    const {version, book} = this.props
-    const chapterArray = Array(+book.chapters).fill().map((_, i) => i+1)
+    const {version, book} = this.props;
+    const chapterArray = Array(+book.chapters).fill().map((_, i) => i+1);
     return (
       <div>
         {chapterArray.map((ch, i) => {
-          return (
-            <Link key={i} to={{
-              pathname: `/${version}/${book.book_name}/${ch}`,
-              state: {
-                version: version,
-                book: book,
-                chapter: ch
-              }
-            }}>
-              {ch}&nbsp;
-            </Link>
-          )
+          // const chapter = (i !== 0 && i % 30 === 0) ? [<br/>, ch] : ch;
+          return <NavLink className={classes.Nav} activeClassName={classes.active} key={`${book.book_id}${i}`} to={{
+            pathname: `/${version}/${book.book_name}/${ch}`,
+            state: {
+              version: version,
+              book: book,
+              chapter: ch
+            }
+          }}>
+            {ch}
+          </NavLink>;
         })}
       </div>
     )
