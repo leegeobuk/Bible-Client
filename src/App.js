d703bf9d939fff360632bab8d5ca4f0715c896e6
leegeobuk <leegeobuk@gmail.com>
2020-08-15 19:18:14 +0900

    Change Homestructure to old and new testaments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Home from "./routes/Home";
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Script from './routes/Script';
 import axios from 'axios';
-import MainNav from './components/MainNav';
+import MainNav from './components/nav/MainNav';
 import About from './routes/About';
+import Auth from './routes/Auth';
 import classes from './App.module.css';
 
 const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alpha/"
@@ -12,28 +13,42 @@ const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alp
 class App extends Component {
   state = {
     version: "KRV",
+    ot: [],
+    nt: [],
     books: []
   }
 
   getBooks = async () => {
-    const books = await axios.get(baseUrl)
-    this.setState(current => ({version: current.version, books: books.data.books}))
+    try {
+      const books = await axios.get(baseUrl);
+      const {ot, nt} = books.data;
+      this.setState(current => ({
+        version: current.version,
+        ot: ot,
+        nt: nt,
+        books: [...ot, ...nt]
+      }))
+    } catch (error) {
+      console.log("error: " + error);
+      // return;
+    }
   }
   
   render() {
-    const {version, books} = this.state
+    const {version, ot, nt, books} = this.state
     return (
       <div className={classes.App}>
-        <HashRouter>
+        <BrowserRouter>
           <MainNav />
           <Route exact path={"/"}>
-            <Home version={version} books={books} />
+            <Home version={version} ot={ot} nt={nt} />
           </Route>
           <Route exact path={"/about"} component={About} />
+          <Route exact path={"/login"} component={Auth} />
           {books.map((book, i) => {
             return <Route path={`/${version}/${book.book_name}/:chapter`} component={Script} key={i} />
           })}
-        </HashRouter>
+        </BrowserRouter>
       </div>
     )
   }
