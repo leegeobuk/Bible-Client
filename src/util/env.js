292ca18e7184ccf351aaaaed71d70342aff64f80
leegeobuk <leegeobuk@gmail.com>
2020-08-15 19:13:52 +0900

    Add environment variables

---
+++ src/util/env.js
@@ -0,0 +1,9 @@
+const API_URL = process.env.REACT_APP_API_BASE_URL;
+const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY;
+const REDIRECT_LOGIN = "http://localhost:3000/login";
+
+export {
+  API_URL,
+  KAKAO_KEY,
+  REDIRECT_LOGIN,
+}
