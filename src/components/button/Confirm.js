c10996f05f69963d966a2a543e0f75feba7c14f6
leegeobuk <leegeobuk@gmail.com>
2020-08-15 19:16:00 +0900

    Add LoginFail modal

---
+++ src/components/button/Confirm.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+import classes from "./Confirm.module.css"
+
+class Confirm extends Component {
+  render() {
+    return (
+      <div className={classes.Confirm}>
+        <button className={classes.confirm_btn} onClick={this.props.toggleDialog}>
+          확인
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Confirm;
---
+++ src/components/modal/LoginFail.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import classes from "./LoginFail.module.css"
+import Confirm from "../button/Confirm";
+
+class LoginFail extends Component {
+  render() {
+    return (
+      <div className={this.props.show ? classes.modal : classes.none}>
+        가입되지 않은 계정입니다
+        <Confirm toggleDialog={this.props.toggleDialog} />
+      </div>
+    );
+  }
+}
+
+export default LoginFail;
