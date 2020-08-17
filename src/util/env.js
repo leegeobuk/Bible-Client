ff2e9dc5b7e8caa311a4386c98def7d4187f72c2
leegeobuk <leegeobuk@gmail.com>
2020-08-17 22:57:24 +0900

    Add Redirect_uri for signup

--- src/util/env.js
+++ src/util/env.js
@@ -1,9 +1,11 @@
 const API_URL = process.env.REACT_APP_API_BASE_URL;
 const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY;
 const REDIRECT_LOGIN = "http://localhost:3000/login";
+const REDIRECT_SIGNUP = "http://localhost:3000/signup";
 
 export {
   API_URL,
   KAKAO_KEY,
   REDIRECT_LOGIN,
+  REDIRECT_SIGNUP,
 }
