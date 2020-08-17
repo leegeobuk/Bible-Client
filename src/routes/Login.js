c62a1f35971e67def738789fecd5e35e2f9d8ec9
leegeobuk <leegeobuk@gmail.com>
2020-08-18 04:28:27 +0900

    Feat Login and redirection

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import { API_URL, KAKAO_KEY, REDIRECT_LOGIN } from "../util/env";
 import axios from "axios";
 import LoginForm from "../components/auth/forms/LoginForm";
-import AuthFail from "../components/modal/AuthFail";
+import AuthDialog from "../components/modal/AuthDialog";
 import Backdrop from "../components/modal/Backdrop";
 import classes from "./Login.module.css";
 
-const authBaseUrl = "https://kauth.kakao.com";
-const authUrl = `${authBaseUrl}/oauth/authorize?client_id=${KAKAO_KEY}&redirect_uri=${REDIRECT_LOGIN}&response_type=code`;
-const tokenUrl = `${API_URL}/login`;
+const kakaoBaseUrl = "https://kauth.kakao.com";
+const kakaoAuthUrl = `${kakaoBaseUrl}/oauth/authorize?client_id=${KAKAO_KEY}&redirect_uri=${REDIRECT_LOGIN}&response_type=code`;
+const kakaoTokenUrl = `${API_URL}/login?type=kakao`;
 
 class Login extends Component {
   state = {
-    isLoginFailed: false
+    dialogOn: false,
+    msg: "",
+    isSigned: false
   }
   
   useQuery = () => {
@@ -21,15 +23,16 @@ class Login extends Component {
   
   login = async (code) => {
     const req = this.createTokenRequest(code);
-    await axios.post(tokenUrl, req)
+    await axios.post(kakaoTokenUrl, req)
       .then(res => {
         console.log(res.status);
-        // redirect to home
+        this.toggleDialog("로그인 되었습니다", true);
       })
       .catch(err => {
         if (err.response.status === 401) {
-          console.log(err.response.status);
-          this.toggleDialog();
+          this.toggleDialog("가입되지 않은 계정입니다");
+        } else if (err.response.status === 500) {
+          this.toggleDialog("서버에 에러가 있었습니다. 다시 시도해 주십시오.");
         }
       });
   }
@@ -44,8 +47,13 @@ class Login extends Component {
     return req;
   }
 
-  toggleDialog = () => {
-    this.setState({isLoginFailed: !this.state.isLoginFailed})
+  toggleDialog = (msg, isSigned = false) => {
+    this.setState({dialogOn: !this.state.dialogOn, msg: msg, isSigned: isSigned});
+  }
+
+  redirect = () => {
+    this.props.markLoggedIn();
+    this.props.history.push("/");
   }
 
   componentDidMount() {
@@ -56,12 +64,12 @@ class Login extends Component {
   }
   
   render() {
-    const {isLoginFailed} = this.state;
+    const {dialogOn, msg, isSigned} = this.state;
     return (
       <div className={classes.Login}>
-        <Backdrop show={isLoginFailed} />
-        <AuthFail show={isLoginFailed} toggle={this.toggleDialog} text="가입되지 않은 계정입니다" />
-        <LoginForm url={authUrl} />
+        <Backdrop show={dialogOn} />
+        <AuthDialog show={dialogOn} click={isSigned ? this.redirect : this.toggleDialog.bind(this, msg)} text={msg} />
+        <LoginForm url={kakaoAuthUrl} />
       </div>
     );
   }
