9282775689e4799f2f8edf89ca76d77aaa803a22
leegeobuk <leegeobuk@gmail.com>
2020-08-18 04:25:39 +0900

    Refactor name and props

--- src/components/modal/AuthDialog.js
+++ src/components/modal/AuthDialog.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import Confirm from "../buttons/Confirm";
-import classes from "./AuthFail.module.css"
+import classes from "./AuthDialog.module.css"
 
-class AuthFail extends Component {
+class AuthDialog extends Component {
   render() {
     return (
       <div className={this.props.show ? classes.modal : classes.none}>
         {this.props.text}
-        <Confirm toggle={this.props.toggle} />
+        <Confirm click={this.props.click} />
       </div>
     );
   }
 }
 
-export default AuthFail;
+export default AuthDialog;
