e66ba3f39d9b6f2f17aee888463c57a315ac037b
leegeobuk <leegeobuk@gmail.com>
2020-08-17 19:36:51 +0900

    Add DivisionBar in LoginForm

---
+++ src/components/auth/DivisionBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./DivisionBar.module.css";
+
+const divisionBar = props => {
+  return <h5 className={classes.DivisionBar}>
+    <span>또는</span>
+  </h5>;
+}
+
+export default divisionBar;
