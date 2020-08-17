c0f14372c7e05429b4d89baa51a9ac9485bbbd90
leegeobuk <leegeobuk@gmail.com>
2020-08-17 22:56:48 +0900

    Refactor name from Auth to Login

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { API_URL, KAKAO_KEY, REDIRECT_LOGIN } from "../util/env";
 import axios from "axios";
 import LoginForm from "../components/auth/forms/LoginForm";
-import LoginFail from "../components/modal/LoginFail";
+import AuthFail from "../components/modal/AuthFail";
 import Backdrop from "../components/modal/Backdrop";
 import classes from "./Login.module.css";
 
@@ -10,7 +10,7 @@ const authBaseUrl = "https://kauth.kakao.com";
 const authUrl = `${authBaseUrl}/oauth/authorize?client_id=${KAKAO_KEY}&redirect_uri=${REDIRECT_LOGIN}&response_type=code`;
 const tokenUrl = `${API_URL}/login`;
 
-class Auth extends Component {
+class Login extends Component {
   state = {
     isLoginFailed: false
   }
@@ -22,9 +22,14 @@ class Auth extends Component {
   login = async (code) => {
     const req = this.createTokenRequest(code);
     await axios.post(tokenUrl, req)
+      .then(res => {
+        console.log(res.status);
+        // redirect to home
+      })
       .catch(err => {
         if (err.response.status === 401) {
           console.log(err.response.status);
+          this.toggleDialog();
         }
       });
   }
@@ -39,7 +44,7 @@ class Auth extends Component {
     return req;
   }
 
-  toggleLoginFailDialog = () => {
+  toggleDialog = () => {
     this.setState({isLoginFailed: !this.state.isLoginFailed})
   }
 
@@ -47,19 +52,19 @@ class Auth extends Component {
     const code = this.useQuery().get('code');
     if (code) {
       this.login(code);
-      this.toggleLoginFailDialog();
     }
   }
   
   render() {
+    const {isLoginFailed} = this.state;
     return (
       <div className={classes.Login}>
-        <Backdrop show={this.state.isLoginFailed} />
-        <LoginFail show={this.state.isLoginFailed} toggleDialog={this.toggleLoginFailDialog} />
+        <Backdrop show={isLoginFailed} />
+        <AuthFail show={isLoginFailed} toggle={this.toggleDialog} text="가입되지 않은 계정입니다" />
         <LoginForm url={authUrl} />
       </div>
     );
   }
 }
 
-export default Auth;
+export default Login;
