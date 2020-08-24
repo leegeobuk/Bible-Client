c46b2d2ec3fef824821a8773060dbbe3d67d254a
leegeobuk <leegeobuk@gmail.com>
2020-08-25 00:22:59 +0900

    Refactor English to Korean

--- src/components/nav/MainNav.js
+++ src/components/nav/MainNav.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import SmallLoginButton from "../auth/buttons/SmallLoginButton";
+import LogoutButton from "../auth/buttons/LogoutButton";
 import classes from "./MainNav.module.css";
 
 class MainNav extends Component {
   render() {
+    const {loggedIn} = this.props;
     return (
       <nav className={classes.main_nav}>
         <div className={classes.nav}>
           <Link className={classes.link} to="/">
-            Home
+            홈
           </Link>
           <Link className={classes.link} to="/about">
-            About
+            감사
           </Link>
         </div>
         <div className={classes.login}>
-          <SmallLoginButton />
+          {}
+          {loggedIn ? <LogoutButton /> : <SmallLoginButton />}
         </div>
       </nav>
     )
