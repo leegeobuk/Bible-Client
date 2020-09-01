b810b2ddfa868928e5567de86641dd43c1eb6be2
leegeobuk <leegeobuk@gmail.com>
2020-09-01 17:04:58 +0900

    Feat silent refresh

--- src/components/auth/AuthChecker.js
+++ src/components/auth/AuthChecker.js
@@ -3,61 +3,45 @@ import { withRouter } from "react-router-dom";
 import MainNav from "../nav/MainNav";
 import { API_URL } from "../../util/env";
 import axios from "axios";
-import Cookies from "universal-cookie";
 
 const kakaoRefreshTokenUrl = `${API_URL}/refreshtoken?type=kakao`;
 
 class AuthChecker extends Component {
-  isTokenValid = (token) => {
-    return token && Date.now() <= token.expiry.getTime();
-  }
-
   shouldRefreshToken = (token) => {
-    if (!token) {
-      return false;
-    }
     const expiry = token.expiry.getTime();
     return Date.now() > expiry;
   }
 
-  listen = (token) => {
-    this.props.history.listen(() => {
-      if (this.shouldRefreshToken(token)) {
-        axios.post(kakaoRefreshTokenUrl, {grantType: "refresh_token"}, {withCredentials: true})
-          .then(res => {
-            console.log(res.data);
-            this.props.setToken(this.tokenize(res.data));
-          })
-          .catch(err => {
-            console.log(err.response);
-            // error handling
-          });
+  onSilentRefresh = async () => {
+    await axios.post(kakaoRefreshTokenUrl, {grantType: "refresh_token"}, {withCredentials: true})
+    .then(res => {
+      console.log(res.data);
+      this.props.setToken(this.props.tokenize(res.data));
+    })
+    .catch(err => {
+      if (err.response.status === 401) {
+        console.log(err.response.data);
+      }
+      else if (err.response.status === 500) {
+        console.log(err.response.data);
       }
     });
   }
-
-  tokenize = (data) => {
-    const date = new Date();
-    date.setSeconds(date.getSeconds() + data.expiresIn);
-    return {
-      accessToken: data.accessToken,
-      expiry: date
-    };
+  
+  componentDidMount() {
+    console.log('initial silent refresh');
+    this.onSilentRefresh();
   }
 
   componentDidUpdate() {
-    
-    // if (refreshToken) {
-      
-    // }
-    this.listen(this.props.token);
-
+    const {token} = this.props;
+    if (this.props.loggedIn && this.shouldRefreshToken(token)) {
+      console.log('silent refresh on token expiry');
+      this.onSilentRefresh(token);
+    }
   }
 
   render() {
-    const cookies = new Cookies();
-    const refreshToken = cookies.get('refresh_token');
-    console.log(refreshToken);
     return <MainNav loggedIn={this.props.loggedIn} />;
   }
 }
