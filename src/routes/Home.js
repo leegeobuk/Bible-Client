01d18dd47409323dcfc7568f25f34de6970dc388
leegeobuk <leegeobuk@gmail.com>
2020-09-01 17:03:07 +0900

    Refactor Books component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import Book from '../components/bible/Book';
+import axios from 'axios';
 import classes from "./Home.module.css";
+import BookLinks from '../components/bible/BookLinks';
+
+const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alpha/"
 
 class Home extends Component {
+  loadBooks = async () => {
+    try {
+      const books = await axios.get(baseUrl);
+      this.props.setBooks(books);
+    } catch (error) {
+      console.log("error: " + error);
+    }
+  }
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+  
   render() {
-    const {version, ot, nt} = this.props
+    const {version, ot, nt} = this.props;
     return (
       <div className={classes.Home}>
         <h1 className={classes.main_theme}>주께로 향하는 삶</h1>
-        <div>
-          구약<br/>
-          {ot.map((book, i) => {
-            return <Book key={i} version={version} book={book} />
-          })}
-          <br/><br/>
-          신약<br/>
-          {nt.map((book, i) => {
-            return <Book key={i} version={version} book={book} />
-          })}
-        </div>
+        <BookLinks version={version} ot={ot} nt={nt} />
       </div>
-    )
+    );
   }
 }
 
