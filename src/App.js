33bb68d87e7cef9fd1dfc68a83eeb5c18b52bb18
leegeobuk <leegeobuk@gmail.com>
2020-09-01 17:02:11 +0900

    Refactor functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import Home from "./routes/Home";
-import { BrowserRouter, Route } from 'react-router-dom';
-import Script from './routes/Script';
-import axios from 'axios';
+import { Route } from 'react-router-dom';
 import About from './routes/About';
 import Login from './routes/Login';
 import Signup from './routes/Signup';
 import AuthChecker from './components/auth/AuthChecker';
+import Script from './routes/Script';
 import classes from './App.module.css';
-
-const baseUrl = "https://6vmwgwqpq0.execute-api.ap-northeast-2.amazonaws.com/alpha/"
 
 class App extends Component {
   state = {
@@ -21,19 +18,16 @@ class App extends Component {
     token: null
   }
 
-  getBooks = async () => {
-    try {
-      const books = await axios.get(baseUrl);
-      const {ot, nt} = books.data;
-      this.setState(current => ({
-        version: current.version,
-        ot: ot,
-        nt: nt,
-        books: [...ot, ...nt]
-      }));
-    } catch (error) {
-      console.log("error: " + error);
-    }
+  setBooks = (books) => {
+    const {ot, nt} = books.data;
+    this.setState(current => ({
+      version: current.version,
+      ot: ot,
+      nt: nt,
+      books: [...ot, ...nt],
+      loggedIn: current.loggedIn,
+      token: current.token
+    }));
   }
 
   isTokenEmpty = (token) => {
@@ -51,37 +45,41 @@ class App extends Component {
     }));
   }
 
-  componentDidMount() {
-    this.getBooks();
+  tokenize = (data) => {
+    const date = new Date();
+    date.setSeconds(date.getSeconds() + data.expiresIn);
+    return {
+      accessToken: data.accessToken,
+      expiry: date
+    };
   }
   
   render() {
     const {version, ot, nt, books, loggedIn, token} = this.state
     return (
       <div className={classes.App}>
-        <BrowserRouter>
-          <AuthChecker token={token} loggedIn={loggedIn} setToken={this.setToken} />
-          <Route exact path={"/"}>
-            <Home version={version} ot={ot} nt={nt} />
-          </Route>
-          <Route exact path={"/about"} component={About} />
-          <Route 
-            exact path={"/login"} 
-            render={props => (
-              <Login 
-                location={props.location} 
-                setToken={this.setToken}
-                {...props} 
-              />
-            )}
-          />
-          <Route exact path={"/signup"} component={Signup} />
-          {books.map((book, i) => {
-            return <Route path={`/${version}/${book.book_name}/:chapter`} component={Script} key={i} />
-          })}
-        </BrowserRouter>
+        <AuthChecker token={token} loggedIn={loggedIn} setToken={this.setToken} tokenize={this.tokenize} />
+        <Route exact path={"/"}>
+          <Home version={version} ot={ot} nt={nt} setBooks={this.setBooks} />
+        </Route>
+        <Route exact path={"/about"} component={About} />
+        <Route 
+          exact path={"/login"} 
+          render={props => (
+            <Login 
+              location={props.location} 
+              setToken={this.setToken}
+              tokenize={this.tokenize}
+              {...props} 
+            />
+          )}
+        />
+        <Route exact path={"/signup"} component={Signup} />
+        {books.map((book, i) => {
+          return <Route key={i} path={`/${version}/${book.book_name}/:chapter`} component={Script} />
+        })}
       </div>
-    )
+    );
   }
 }
 
