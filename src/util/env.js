b252634fbe9de9b6885e6b76f0ff4e38c3b9be61
leegeobuk <leegeobuk@gmail.com>
2020-09-02 18:30:49 +0900

    Add redirect_uri to home

--- src/util/env.js
+++ src/util/env.js
@@ -1,11 +1,13 @@
 const API_URL = process.env.REACT_APP_API_BASE_URL;
 const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY;
-const REDIRECT_LOGIN = "http://localhost:3000/login";
+const REDIRECT_HOME = "http://localhost:3000/";
 const REDIRECT_SIGNUP = "http://localhost:3000/signup";
+const REDIRECT_LOGIN = "http://localhost:3000/login";
 
 export {
   API_URL,
   KAKAO_KEY,
-  REDIRECT_LOGIN,
+  REDIRECT_HOME,
   REDIRECT_SIGNUP,
+  REDIRECT_LOGIN,
 }
