30b384f99b552bfbf78b89c49d76e02ba7fafb7b
leegeobuk <leegeobuk@gmail.com>
2020-09-07 18:15:02 +0900

    Fix button type

--- src/components/auth/buttons/AuthButton.js
+++ src/components/auth/buttons/AuthButton.js
@@ -3,7 +3,7 @@ import classes from "./AuthButton.module.css";
 
 const authButton = props => {
   const classList = [classes.auth_btn, props.hasMargin ? classes.margin_btn : ''];
-  return <input className={classList.join(' ')} type="submit" value={props.value} />;
+  return <input className={classList.join(' ')} type="button" value={props.value} onClick={props.submit} />;
 }
 
 export default authButton;
