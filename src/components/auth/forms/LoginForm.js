4ff8b8a9e24b85bb1d77e7e0ccd1e688846ce094
leegeobuk <leegeobuk@gmail.com>
2020-09-07 18:17:28 +0900

    Fix miscellaneous things

--- src/components/auth/forms/LoginForm.js
+++ src/components/auth/forms/LoginForm.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import LabeledInput from "./LabeledInput";
-import SignupLoginButton from "../buttons/AuthButton";
-import axios from "axios";
+import AuthButton from "../buttons/AuthButton";
 import SignupLink from "../SignupLink";
 import DivisionBar from "../DivisionBar";
+// import axios from "axios";
 import classes from "./LoginForm.module.css";
 
 const loginImage = "/images/login_kakao.png";
@@ -33,7 +33,7 @@ class LoginForm extends Component {
         <form className={classes.auth} onSubmit={this.handleSubmit}>
           <LabeledInput label="이메일" onChange={this.handleEmailChange} />
           <LabeledInput label="비밀번호" onChange={this.handlePasswordChange} />
-          <SignupLoginButton value="로그인" />
+          <AuthButton value="로그인" />
         </form>
         <SignupLink />
         <DivisionBar />
