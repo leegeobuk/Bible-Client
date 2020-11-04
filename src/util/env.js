4671ab8531d5e3cb646867636b857fd941178143
leegeobuk <leegeobuk@gmail.com>
2020-11-04 15:49:43 +0900

    Add redirect_uris for production

--- src/util/env.js
+++ src/util/env.js
@@ -1,12 +1,18 @@
 const API_URL = process.env.REACT_APP_API_BASE_URL;
 const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY;
-const REDIRECT_HOME = "http://localhost:3000/";
-const REDIRECT_SIGNUP = "http://localhost:3000/signup";
-const REDIRECT_LOGIN = "http://localhost:3000/login";
+const REDIRECT_TEST_HOME = "http://localhost:3000/";
+const REDIRECT_TEST_SIGNUP = "http://localhost:3000/signup";
+const REDIRECT_TEST_LOGIN = "http://localhost:3000/login";
+const REDIRECT_HOME = "https://www.biblennium.com/";
+const REDIRECT_SIGNUP = "https://www.biblennium.com/signup";
+const REDIRECT_LOGIN = "https://www.biblennium.com/login";
 
 export {
   API_URL,
   KAKAO_KEY,
+  REDIRECT_TEST_HOME,
+  REDIRECT_TEST_SIGNUP,
+  REDIRECT_TEST_LOGIN,
   REDIRECT_HOME,
   REDIRECT_SIGNUP,
   REDIRECT_LOGIN,
